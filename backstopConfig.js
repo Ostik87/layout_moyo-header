1b7d94e91c8fd315e468ec9a5886a481d245d556
Ostik87 <oskarkit2@gmail.com>
2026-03-25 00:09:46 +0200

    fixx

--- backstopConfig.js
+++ backstopConfig.js
@@ -1,13 +1,11 @@
 'use strict';
-// https://github.com/garris/BackstopJS#advanced-scenarios
-
 const backstop = require('@mate-academy/backstop-config');
 const { basicScenario } = backstop;
 
 const basic = {
   ...basicScenario,
   label: 'Elementary test',
-  referenceUrl: basicScenario.referenceUrl + '/moyo-header/',
+  url: 'http://localhost:3000/index.html', // 🔹 ось сюди твій локальний сервер
 };
 
 const config = {
@@ -16,42 +14,21 @@ const config = {
   onBeforeScript: 'puppet/onBefore.js',
   onReadyScript: 'puppet/onReady.js',
   viewports: [
-    {
-      name: '1024px',
-      width: 1024,
-      height: 768,
-    },
-    {
-      name: '1200px',
-      width: 1200,
-      height: 768,
-    },
+    { name: '1024px', width: 1024, height: 768 },
+    { name: '1200px', width: 1200, height: 768 },
   ],
   scenarios: [
-    {
-      ...basic,
-      label: 'Header tag',
-      selectors: ['header'],
-    },
-    {
-      ...basic,
-      label: 'Nav tag',
-      selectors: ['nav'],
-    },
+    { ...basic, label: 'Header tag', selectors: ['header'] },
+    { ...basic, label: 'Nav tag', selectors: ['nav'] },
     {
       ...basic,
       misMatchThreshold: 2,
       label: 'Link with data-qa_hover',
       selectors: ['[data-qa="hover"]'],
       hoverSelector: '[data-qa="hover"]',
-      postInteractionWait: 1000,
+      postInteractionWait: 2000,
     },
-    {
-      ...basic,
-      misMatchThreshold: 2,
-      label: 'Link with class_is-active',
-      selectors: ['a.is-active'],
-    },
+    { ...basic, misMatchThreshold: 2, label: 'Link with class_is-active', selectors: ['a.is-active'] },
   ],
 };
 
